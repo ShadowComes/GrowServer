0fc7de77af1760d535fa125814dc3eac85839041
Gabriēls Zīverts <zikmanis1@gmail.com>
2026-01-21 17:22:18 +0200

    fix: change hostname to 0.0.0.0 for server accessibility

--- apps/logon/src/index.ts
+++ apps/logon/src/index.ts
@@ -42,7 +42,7 @@ async function init() {
           cert: ssl.tls.cert,
         },
         port: config.web.port,
-        hostname: config.web.address,
+        hostname: "0.0.0.0",
       },
       (info) => {
         logger.info(`Node Logon Server is running on port ${info.port}`);
